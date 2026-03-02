b90ab4bf8e1569f32e465a9c052bb67f7af96716
Anand Narayana Perumal <anand@Anands-MacBook-Pro-7917.local>
2026-03-02 01:36:49 +0100

    MainTest cass modified

--- api-client-java/src/test/java/com/aixigo/sample_projects/api_client_java/MainTest.java
+++ api-client-java/src/test/java/com/aixigo/sample_projects/api_client_java/MainTest.java
@@ -1,13 +1,27 @@
 package com.aixigo.sample_projects.api_client_java;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
   @Test
-  void simpleSanityTest() {
-    int result = 2 + 3;
-    assertEquals(5, result);
+  void projectHasMainClass() {
+    // just checks the class exists & loads
+    assertDoesNotThrow(() -> Class.forName("com.aixigo.sample_projects.api_client_java.Main"));
+  }
+
+  @Test
+  void ensureNotNaNThrowsOnNaN() throws Exception {
+    var m = Main.class.getDeclaredMethod("ensureNotNaN", double.class, String.class);
+    m.setAccessible(true);
+
+    // should not throw
+    assertDoesNotThrow(() -> m.invoke(null, 1.0, "ok"));
+
+    // should throw
+    var ex = assertThrows(Exception.class, () -> m.invoke(null, Double.NaN, "bad"));
+    assertNotNull(ex);
   }
 }
